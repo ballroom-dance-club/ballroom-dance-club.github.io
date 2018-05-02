d24c7631ec8677323e0c25abadfd43938832b132
Dane Hillard <github@danehillard.com>
2018-05-02 00:10:21 -0400

    Replace glyphicons with fontawesome

--- js/site.js
+++ js/site.js
@@ -1,9 +1,3 @@
-$(".collapse").on("show.bs.collapse", function(){
-    $(this).parent().find(".glyphicon-triangle-right").removeClass("glyphicon-triangle-right").addClass("glyphicon-triangle-bottom");
-}).on("hidden.bs.collapse", function(){
-    $(this).parent().find(".glyphicon-triangle-bottom").removeClass("glyphicon-triangle-bottom").addClass("glyphicon-triangle-right");
-});
-
 function trackEvent(action, category, label){
     if (typeof ga !== "undefined"){
         ga("send", {
@@ -20,7 +14,7 @@ $("[data-track]").on("click", function(){
         trackingFields = null,
         trackingCategory = null,
         trackingLabel = null;
-    
+
     if (trackValue.match(/([\w ]+):([\w ]+)/)){
         trackingFields = trackValue.split(":");
         trackingCategory = trackingFields[0];
